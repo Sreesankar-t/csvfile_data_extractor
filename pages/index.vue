d1699a15798be7e0cdca3449524d4963b4c0a8a0
sreesanker <sreeshankerthekkan@gmail.com>
2024-04-11 21:02:41 +0530

    added style

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="main">
+    <h2>please select you .csv file</h2>
     <input type="file" @change="handleFileChange" />
     <div v-if="isLoading">
       <h1>Loading....</h1>
@@ -32,3 +33,22 @@ export default {
   }
 }
 </script>
+<style scoped>
+ .main{
+  background-color: rgb(182, 241, 241);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 30px;
+  flex-direction: column;
+ }
+ .main h2{
+  font-size: 22px;
+  font-weight: 600;
+ }
+ .main input{
+   border: 1px solid goldenrod;
+   padding: 20px;
+ }
+</style>
